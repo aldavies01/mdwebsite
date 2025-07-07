f5e0ea7aa1b47df21c4fee3c817134e5b3a7d949
aldavies01 <a.ldavies@hotmail.co.uk>
2025-07-07 11:06:35 +0100

    updated referral form follwoing debug

--- src/pages/ReferralFormPage.jsx
+++ src/pages/ReferralFormPage.jsx
@@ -1,19 +1,9 @@
-// import ReferralForm from "../components/referrals/ReferralForm";
-
-// function ReferralFormPage() {
-//   return (
-//     <div className="min-h-screen bg-gray-50 py-12">
-//       <ReferralForm />
-//     </div>
-//   );
-// }
-
-// export default ReferralFormPage;
+import ReferralForm from "../components/referrals/ReferralForm";
 
 function ReferralFormPage() {
   return (
     <div className="min-h-screen bg-gray-50 py-12">
-      <h1>Referral Form Page Works!</h1>
+      <ReferralForm />
     </div>
   );
 }
